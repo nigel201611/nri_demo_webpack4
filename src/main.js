cb54e27c286666b4eddd98acf6ea44b8240978ca
nigel <1149828479@qq.com>
2020-12-22 10:16:27 +0800

    refactor:结构优化

--- src/main.js
+++ src/main.js
@@ -1,35 +1,35 @@
 /*
  * @Author: nigel
  * @Date: 2020-05-12 14:33:04
- * @LastEditTime: 2020-05-20 13:50:44
+ * @LastEditTime: 2020-12-22 10:15:26
  */
 /* eslint-disable */
-import babelpolyfill from 'babel-polyfill';
-import Vue from 'vue';
-import VueI18n from 'vue-i18n';
-import App from './App';
-import ElementUI from 'element-ui';
-import enLocale from 'element-ui/lib/locale/lang/en';
-import jaLocale from 'element-ui/lib/locale/lang/ja';
-import zhLocale from 'element-ui/lib/locale/lang/zh-CN';
+import babelpolyfill from "babel-polyfill";
+import Vue from "vue";
+import VueI18n from "vue-i18n";
+import App from "./App";
+import ElementUI from "element-ui";
+import enLocale from "element-ui/lib/locale/lang/en";
+import jaLocale from "element-ui/lib/locale/lang/ja";
+import zhLocale from "element-ui/lib/locale/lang/zh-CN";
 import Exif from "exif-js";
 
 // import 'element-ui/lib/theme-default/index.css'
-import 'element-ui/lib/theme-chalk/index.css';
+import "element-ui/lib/theme-chalk/index.css";
 // import './assets/theme/theme-green/index.css'
-import VueRouter from 'vue-router';
-import store from './vuex/store';
-import Vuex from 'vuex';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
-import routes from './routes';
-import './styles/main.scss';
-import 'font-awesome/css/font-awesome.min.css';
+import VueRouter from "vue-router";
+import store from "./vuex/store";
+import Vuex from "vuex";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import routes from "./routes";
+import "./styles/main.scss";
+import "font-awesome/css/font-awesome.min.css";
 
 // 启动模拟接口
 // import Mock from './mock/index.js'
 // Mock.bootstrap();
-import utils from './common/js/util.js';
+import utils from "./common/js/util.js";
 window.storeLocal = utils.storeLocal;
 window.storeSession = utils.storeSession;
 window.Exif = Exif;
@@ -42,36 +42,41 @@ Vue.use(Vuex);
 NProgress.configure({ showSpinner: false });
 const router = new VueRouter({
   // mode: 'history',
-  routes
+  routes,
 });
 
 const i18n = new VueI18n({
-  locale: 'zh-CH', // set locale
+  locale: "zh-CH", // set locale
   messages: {},
   // silentFallbackWarn: true,
   // silentTranslationWarn: true
 });
 
-
 router.beforeEach((to, from, next) => {
   NProgress.start();
   /*需要判断登录*/
-  let token = storeSession.get('token');
-  if (!token && to.name !== 'login') {
+  let token = storeSession.get("token");
+  if (!token && to.name !== "login") {
     next({
-      path: '/login',
-      query: { redirect: to.fullPath }
+      path: "/login",
+      query: { redirect: to.fullPath },
     });
   } else {
     let locale = store.state.menuStore.locals;
     i18n.locale = locale;
     switch (locale) {
-      case 'zh-CH': Vue.use(ElementUI, zhLocale); break;
-      case 'en': Vue.use(ElementUI, enLocale); break;
-      case 'ja': Vue.use(ElementUI, jaLocale); break;
+      case "zh-CH":
+        Vue.use(ElementUI, zhLocale);
+        break;
+      case "en":
+        Vue.use(ElementUI, enLocale);
+        break;
+      case "ja":
+        Vue.use(ElementUI, jaLocale);
+        break;
     }
-    if (to.path == '/login') {
-      sessionStorage.removeItem('user');
+    if (to.path == "/login") {
+      sessionStorage.removeItem("user");
     }
     next();
   }
@@ -96,7 +101,7 @@ const rootVue = new Vue({
   router,
   store,
   i18n,
-  render: h => h(App)
-}).$mount('#app');
+  render: (h) => h(App),
+}).$mount("#app");
 
 export default rootVue;
--- src/routes.js
+++ src/routes.js
@@ -1,102 +1,105 @@
 /*
  * @Author: nigel
  * @Date: 2020-03-19 17:42:35
- * @LastEditTime: 2020-07-20 16:13:39
+ * @LastEditTime: 2020-12-22 10:15:16
  */
-import Login from './views/login/login.vue';
-import NotFound from './views/404.vue';
-import Home from './views/Home.vue';
-import eslUpdate from './views/eslUpdate/eslUpdate.vue';
-import ocr from './views/ocr/ocr.vue';
-import postCodeOcr from './views/postCodeOcr/postCodeOcr.vue';
-import dectectApi from './views/dectectApi/dectectApi.vue';
-import drawImg from './views/drawImg/drawImg.vue';
-import generalocr from './views/generalocr/generalocr.vue';
-import googleOcr from './views/googleOcr/googleOcr.vue';
-import customizeOcr from './views/customizeOcr/customizeOcr.vue';
-import myOcrTemplate from './views/myOcrTemplate/myOcrTemplate.vue';
+import Login from "./views/login/login.vue";
+import NotFound from "./views/404.vue";
+import Home from "./views/Home.vue";
+import eslUpdate from "./views/eslUpdate/eslUpdate.vue";
+import ocr from "./views/ocr/ocr.vue";
+import postCodeOcr from "./views/postCodeOcr/postCodeOcr.vue";
+import dectectApi from "./views/dectectApi/dectectApi.vue";
+import drawImg from "./views/drawImg/drawImg.vue";
+import generalocr from "./views/generalocr/generalocr.vue";
+import googleOcr from "./views/googleOcr/googleOcr.vue";
+import customizeOcr from "./views/customizeOcr/customizeOcr.vue";
+import myOcrTemplate from "./views/myOcrTemplate/myOcrTemplate.vue";
 // import gesture from './views/gesture/gesture.vue';
-let routes = [{
-    path: '/login',
+let routes = [
+  {
+    path: "/login",
     component: Login,
-    name: 'login',
-},
-{
-    path: '/404',
+    name: "login",
+  },
+  {
+    path: "/404",
     component: NotFound,
-    name: '',
-},
-{
-    path: '/',
+    name: "",
+  },
+  {
+    path: "/",
     component: Home,
-    name: 'Home',
-    children: [{
-        path: '/eslUpdate',
+    name: "Home",
+    children: [
+      {
+        path: "/eslUpdate",
         component: eslUpdate,
-        name: 'eslUpdate',
-        alias: 'esl更新'
-    },
-    {
-        path: '/ocr',
+        name: "eslUpdate",
+        alias: "esl更新",
+      },
+      {
+        path: "/ocr",
         component: ocr,
-        name: 'ocr',
-        alias: '运单识别'
-    },
-    {
-        path: '/dectectApi',
+        name: "ocr",
+        alias: "运单识别",
+      },
+      {
+        path: "/dectectApi",
         component: dectectApi,
-        name: 'dectectApi',
-        alias: '智能识别API'
-    },
-    {
-        path: '/postCodeOcr',
+        name: "dectectApi",
+        alias: "智能识别API",
+      },
+      {
+        path: "/postCodeOcr",
         component: postCodeOcr,
-        name: 'postCodeOcr',
-        alias: '邮编识别'
-    },
-    {
-        path: '/drawImg',
+        name: "postCodeOcr",
+        alias: "邮编识别",
+      },
+      {
+        path: "/drawImg",
         component: drawImg,
-        name: 'drawImg',
-        alias: '指定区域の画像認識'
-    },
-    {
-        path: '/customizeOcr',
+        name: "drawImg",
+        alias: "指定区域の画像認識",
+      },
+      {
+        path: "/customizeOcr",
         component: customizeOcr,
-        name: 'customizeOcr',
-        alias: '自定区域OCR'
-    },
-    {
-        path: '/generalocr',
+        name: "customizeOcr",
+        alias: "自定区域OCR",
+      },
+      {
+        path: "/generalocr",
         component: generalocr,
-        name: 'generalocr',
-        alias: '通用印刷体文字识别'
-    },
-    // {
-    //     path: '/gesture',
-    //     component: gesture,
-    //     name: 'gesture',
-    //     alias: '手势识别'
-    // },
-    {
-        path: '/googleOcr',
+        name: "generalocr",
+        alias: "通用印刷体文字识别",
+      },
+      // {
+      //     path: '/gesture',
+      //     component: gesture,
+      //     name: 'gesture',
+      //     alias: '手势识别'
+      // },
+      {
+        path: "/googleOcr",
         component: googleOcr,
-        name: 'googleOcr',
-        alias: '谷歌通用文字识别'
-    }, {
-        path: '/myOcrTemplate',
+        name: "googleOcr",
+        alias: "谷歌通用文字识别",
+      },
+      {
+        path: "/myOcrTemplate",
         component: myOcrTemplate,
-        name: 'myOcrTemplate',
-        alias: '我的模板'
-    }
-    ]
-},
-{
-    path: '*',
+        name: "myOcrTemplate",
+        alias: "我的模板",
+      },
+    ],
+  },
+  {
+    path: "*",
     redirect: {
-        path: '/404'
-    }
-}
+      path: "/404",
+    },
+  },
 ];
 
 export default routes;
